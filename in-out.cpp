cc9c8bdb68ac5ee416aa3acfc8ca8306851e8605
Niccii03 <104451118+Niccii03@users.noreply.github.com>
2023-02-15 20:23:55 +0300

    п.5

---
+++ in-out.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <math.h> 
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "x = "; cin >> x; // Подсказка и ввод х
+	cout << "y = "; cin >> y; // Подсказка и ввод y
+	cout << x << " + " << y << " = " << x + y << endl;
+	cout << x << " - " << y << " = " << x - y << endl;
+	cout << x << " * " << y << " = " << x * y << endl;
+	cout << x << " / " << y << " = " << x / y << endl;
+	cout << x << " % " << y << " = " << x % y << endl;
+	cout << x << " ^ " << y << " = " << pow(x,y) << endl;
+}
+	
